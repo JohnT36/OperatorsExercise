ebc9ef79b5ccda7374b41830efeca56196fe4408
John Tuck <j.tuck1336@gmail.com>
2022-06-02 09:31:08 -0400

    operators exercise

---
+++ Operators Exercise/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Operators_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 20;
+            var b = 3;
+
+            var div = a / b;
+            Console.WriteLine(div);
+
+            var mod = a % b;
+            Console.WriteLine(mod);
+
+            Console.WriteLine($"{a}/{b} is {div} remainder of {mod}");
+
+            var rad = 20;
+            var pi = Math.PI;
+            var A = pi * (rad * rad);
+            Console.WriteLine(A);
+
+            var i = 3;
+            var j = 4;
+
+            var k = ++i * j++;
+            Console.WriteLine(k);
+
+            var areaOfCircle = CalculateArea(rad);
+
+            Console.WriteLine($"The area of a circle with the radius of {rad} is {CalculateArea(rad)}");   
+        }
+
+
+            public static double CalculateArea(double radius)
+            {
+               return Math.PI * (radius * radius); 
+                
+            }
+
+        
+    }
+}
